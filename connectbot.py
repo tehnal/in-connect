28b6fbcd4bf7f9f7dead9d9cacd7e508afc53d46
tehnal <calikid1002@gmail.com>
2025-08-20 15:48:28 -0700

    Create connectbot.py

---
+++ connectbot.py
@@ -0,0 +1,16 @@
+import pyautogui, time, functions as func
+
+pyautogui.rightClick(260, 1021)
+pyautogui.click(322, 822)
+pyautogui.click(319, 83)
+time.sleep(.5)
+pyautogui.typewrite('linkedin.com/mynetwork/grow')
+pyautogui.press('enter')
+time.sleep(6)
+pyautogui.click(1336, 517)
+time.sleep(4)
+
+
+    
+func.tester()
+
